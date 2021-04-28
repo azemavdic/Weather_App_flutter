dffcc22f0cb3d0e7e5aeb7b92becf2a93428f4d7
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-04-29 00:02:48 +0200

    http package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:core';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -20,8 +23,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    Uri uri = Uri.https('samples.openweathermap.org', 'data/2.5/weather', {
+      'lat': '44.7075',
+      'lon': '18.4919',
+      'appid': '27ea5b71f4a79004a0c688852d1bcdc2'
+    });
+    http.Response response = await http.get(uri);
+    try {
+      print(response.body);
+    } catch (e) {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
 }
+//http://api.openweathermap.org/data/2.5/weather?q=srebrenik&appid=27ea5b71f4a79004a0c688852d1bcdc2
